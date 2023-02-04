39dd1112923371d91c5cd07e283e77b5c5c783ec
EduardSekan <sekaneduard@gmail.com>
2023-02-04 17:38:50 +0200

    add hw 7

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://63dd0cbcdf83d549ce9977d1.mockapi.io';
+axios.defaults.baseURL = 'https://63dd0cbcdf83d549ce9977d1.mockapi.io/contacts/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
